afd6bfe5ee32da1cdbe1c712da06e2cdeaa23250
Linas <stankunaslin0@gmail.com>
2020-08-10 19:43:14 +0300

    Exceptionai

--- src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    Project findByProjectIdentifier(String project);
+
     @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Iterable<Project> findAll();
 }
--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -19,6 +19,30 @@ public class ProjectService {
         }catch (Exception e){
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
         }
+    }
 
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectid){
+        Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Cannot delete non existing project with ID: "+projectid);
+        }
+
+        projectRepository.delete(project);
     }
 }
